72c94fb927a3072053cca37122e88f019e0723b7
Seu Nome <seu@email.com>
2026-08-05 15:04:46 -0300

    Faxina: removendo arquivos do sistema e temporários

--- tmp_check.py
+++
@@ -1,6 +0,0 @@
-import sys
-import importlib.metadata as m
-print('python', sys.executable)
-print('streamlit', m.version('streamlit'))
-print('starlette', m.version('starlette'))
-print('uvicorn', m.version('uvicorn'))
